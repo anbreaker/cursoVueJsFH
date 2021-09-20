552683c1b01407430cfc8a3c52d0a3a01a778e12
anbreaker <antunez19@gmail.com>
2021-09-20 13:39:10 +0200

    first components, starting basic structure of the project

---
+++ 07-journal-app/src/modules/daybook/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav class="nav bg-primary">
+      <a class="navbar-brand text-white">
+        <img
+          src="@/assets/logo.png"
+          alt="Vue logo"
+          class="d-inline-block align-text-top mx-2"
+          height="24"
+        />
+        Daybook
+      </a>
+
+      <div class="d-flex">
+        <button class="btn btn-outline-info mx-2">
+          <i class="fa fa-sign-out-alt"></i>
+        </button>
+      </div>
+    </nav>
+  </div>
+</template>
