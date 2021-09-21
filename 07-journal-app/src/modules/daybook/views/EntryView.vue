c7acfc915f2a3457dec84711c9c274ddc6e0bec1
anbreaker <antunez19@gmail.com>
2021-09-21 18:02:49 +0200

    rutas anidadas

---
+++ 07-journal-app/src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="entry-title d-flex justify-content-between p-2">
+    <div>
+      <span class="text-success fs-3 fw-bold">15</span>
+      <span class="mx-1 fs-3">September</span>
+      <span class="mx-2 fs-4 fw-light">21</span>
+    </div>
+
+    <div>
+      <button class="btn btn-danger mx-2">
+        Delete
+        <i class="fa fa-trash-alt"></i>
+      </button>
+      <button class="btn btn-primary">
+        Update Image
+        <i class="fa fa-upload"></i>
+      </button>
+    </div>
+  </div>
+
+  <hr />
+
+  <div class="d-flex flex-column px-3 h-75">
+    <textarea placeholder="what happened today?"></textarea>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
