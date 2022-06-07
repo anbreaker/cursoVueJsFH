f7ee1623ccb0855cdd740f16d070f39adce8e41a
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-06-07 18:07:22 +0200

    PokemonPage test

--- 04-pokemon-game-Cli-5.0/tests/unit/pages/PokemonPage.spec.js
+++ 04-pokemon-game-Cli-5.0/tests/unit/pages/PokemonPage.spec.js
@@ -1,6 +1,7 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 
 import PokemonPage from '@/pages/PokemonPage'
+import { pokemonArr } from '../mocks/pokemons.mock'
 
 describe('PokemonPage Component', () => {
   let wrapper
@@ -8,7 +9,7 @@ describe('PokemonPage Component', () => {
   beforeEach(() => {
     wrapper = shallowMount(PokemonPage)
   })
-  test('should first', () => {
+  test('Should match the snapshot', () => {
     expect(wrapper.html()).toMatchSnapshot()
   })
 
@@ -22,5 +23,44 @@ describe('PokemonPage Component', () => {
     shallowMount(PokemonPage)
 
     expect(mixPokemonArraySpy).toHaveBeenCalled()
+  })
+
+  test('Should match the snapshot, when pokemons is loaded', () => {
+    const wrapper = shallowMount(PokemonPage, {
+      data() {
+        return {
+          pokemonArr: pokemonArr,
+          pokemon: pokemonArr[0],
+          showPokemon: false,
+          showAnswer: false,
+          message: '',
+        }
+      },
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('Must show the components PokemonPicture and PokemonOptions', () => {
+    const wrapper = shallowMount(PokemonPage, {
+      data() {
+        return {
+          pokemonArr: pokemonArr,
+          pokemon: pokemonArr[0],
+          showPokemon: false,
+          showAnswer: false,
+          message: '',
+        }
+      },
+    })
+
+    const picture = wrapper.find('pokemonpicture-stub')
+    const options = wrapper.find('pokemonoptions-stub')
+
+    expect(picture.exists()).toBeTruthy()
+    expect(options.exists()).toBeTruthy()
+
+    expect(picture.attributes('pokemonid')).toBe('5')
+    expect(options.attributes('pokemons')).toBeTruthy()
   })
 })
--- 04-pokemon-game/src/pages/PokemonPage.vue
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -18,67 +18,66 @@
 </template>
 
 <script>
-import PokemonOptions from "@/components/PokemonOptions";
-import PokemonPicture from "@/components/PokemonPicture";
+  import PokemonOptions from '@/components/PokemonOptions';
+  import PokemonPicture from '@/components/PokemonPicture';
 
-import getPokemonOptions from "@/helpers/getPokemonOptions";
+  import getPokemonOptions from '@/helpers/getPokemonOptions';
 
-export default {
-  name: "PokemonPage",
-  components: {
-    PokemonPicture,
-    PokemonOptions
-  },
-
-  data() {
-    return {
-      pokemonArr: [],
-      pokemon: null,
-      showPokemon: false,
-      showAnswer: false,
-      message: ""
-    };
-  },
-
-  methods: {
-    async mixPokemonArray() {
-      this.pokemonArr = await getPokemonOptions();
-
-      const rndInt = Math.floor(Math.random() * 4);
-
-      this.pokemon = this.pokemonArr[rndInt];
+  export default {
+    name: 'PokemonPage',
+    components: {
+      PokemonPicture,
+      PokemonOptions,
     },
 
-    checkAnswer(selectedId) {
-      this.showPokemon = true;
-      this.showAnswer = true;
-
-      if (selectedId === this.pokemon.id)
-        this.message = `Correct!, is ${this.pokemon.name}`;
-      else
-        this.message = `Opps!, the correct pokemon is ${this.pokemon.name.replace(
-          /^\w/,
-          c => c.toUpperCase()
-        )}`;
+    data() {
+      return {
+        pokemonArr: [],
+        pokemon: null,
+        showPokemon: false,
+        showAnswer: false,
+        message: '',
+      };
     },
 
-    newGame() {
-      this.pokemonArr = [];
-      this.showPokemon = false;
-      this.showAnswer = false;
-      this.pokemon = null;
+    methods: {
+      async mixPokemonArray() {
+        this.pokemonArr = await getPokemonOptions();
+
+        const rndInt = Math.floor(Math.random() * 4);
+
+        this.pokemon = this.pokemonArr[rndInt];
+      },
+
+      checkAnswer(selectedId) {
+        this.showPokemon = true;
+        this.showAnswer = true;
+
+        if (selectedId === this.pokemon.id)
+          this.message = `Correct!, is ${this.pokemon.name}`;
+        else
+          this.message = `Opps!, the correct pokemon is ${this.pokemon.name.replace(
+            /^\w/,
+            (c) => c.toUpperCase()
+          )}`;
+      },
+
+      newGame() {
+        this.pokemonArr = [];
+        this.showPokemon = false;
+        this.showAnswer = false;
+        this.pokemon = null;
+        this.mixPokemonArray();
+      },
+    },
+
+    mounted() {
       this.mixPokemonArray();
-    }
-  },
-
-  mounted() {
-    this.mixPokemonArray();
-  },
-  beforeUnmount() {
-    this.newGame();
-  }
-};
+    },
+    beforeUnmount() {
+      this.newGame();
+    },
+  };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style scoped></style>
