a637b40cad10e457c9cba67e2a4bff55f638f6c8
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-06-14 12:04:29 +0200

    test with wrappres

--- 07-journal-app-cli-5.0/tests/unit/modules/daybook/components/Fab.spec.js
+++ 07-journal-app-cli-5.0/tests/unit/modules/daybook/components/Fab.spec.js
@@ -3,13 +3,35 @@ import { shallowMount } from '@vue/test-utils';
 import Fab from '@/modules/daybook/components/Fab.vue';
 
 describe('Fab Component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(Fab);
+  });
   test('should display the default icon', () => {
     // fa-plus
+    const icon = wrapper.find('i');
+
+    expect(icon.classes()).toContain('fa-plus');
   });
+
   test('must show the icon for each argument', () => {
     // fa-circle
+    const wrapper = shallowMount(Fab, {
+      props: {
+        icon: 'fa-circle',
+      },
+    });
+
+    const icon = wrapper.find('i');
+    expect(icon.classes()).toContain('fa-circle');
   });
+
   test('must emit the on:click event on click', () => {
     // wrapper.emmited('on:click')
+
+    wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted('on:click')).toHaveLength(1);
   });
 });
