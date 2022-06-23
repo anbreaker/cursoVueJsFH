fcde70f800940cde738813932720837683a67646
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-06-23 14:25:12 +0200

    validation geolacation

--- 11-maps-vue-app/src/main.ts
+++ 11-maps-vue-app/src/main.ts
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-createApp(App).use(store).use(router).mount('#app')
+if (!navigator.geolocation) {
+  alert('Your browser does not support geolocation.');
+
+  throw new Error('Your browser does not support geolocation.');
+}
+
+createApp(App).use(store).use(router).mount('#app');
