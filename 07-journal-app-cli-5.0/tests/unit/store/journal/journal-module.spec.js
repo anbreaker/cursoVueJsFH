32df12cfe955747d045c1954af4a2f3a5a170865
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-06-27 17:46:58 +0200

    test updateEntry

--- 07-journal-app-cli-5.0/tests/unit/store/journal/journal-module.spec.js
+++ 07-journal-app-cli-5.0/tests/unit/store/journal/journal-module.spec.js
@@ -15,7 +15,6 @@ const createVuexStore = (initialState) =>
 
 describe('Vuex - Testing Journal Module', () => {
   //Basics
-
   test('Init State should be initialized', () => {
     const store = createVuexStore(journalState);
 
@@ -23,5 +22,37 @@ describe('Vuex - Testing Journal Module', () => {
 
     expect(isLoading).toBeFalsy();
     expect(entries).toEqual(journalState.entries);
+  });
+
+  // Mutation
+  test('mutation: setEntries', () => {
+    const store = createVuexStore({ isLoading: true, entries: [] });
+
+    store.commit('journal/setEntries', journalState.entries);
+    expect(store.state.journal.entries.length).toBe(2);
+
+    store.commit('journal/setEntries', journalState.entries);
+    expect(store.state.journal.entries.length).toBe(4);
+
+    expect(store.state.journal.isLoading).toBeFalsy();
+  });
+
+  test('mutation: updateEntry', () => {
+    const store = createVuexStore(journalState);
+
+    const updateEntry = {
+      id: '-N4hKD2-n_7O-uv1REHY',
+      date: 1627077227978,
+      text: 'Hello Mock Data test +info testing',
+    };
+
+    store.commit('journal/updateEntry', updateEntry);
+
+    expect(store.state.journal.entries.length).toBe(2);
+    // expect(store.state.journal.entries[0]).toEqual(updateEntry);
+
+    expect(
+      store.state.journal.entries.find((entry) => entry.id === updateEntry.id)
+    ).toEqual(updateEntry);
   });
 });
