acfa3e2411da144a2b3e9ccd63d34a8dccba8bf1
Francisco Javier Antunez Duran <francisco.javier.antunez.duran@ibm.com>
2022-07-04 09:20:30 +0200

    mutations test

--- 09-journal-option-api-vs-composition-api/tests/unit/modules/auth/store/auth-module.spec.js
+++ 09-journal-option-api-vs-composition-api/tests/unit/modules/auth/store/auth-module.spec.js
@@ -16,4 +16,50 @@ describe('Vuex, test AuthModule', () => {
     expect(idToken).toBe(null);
     expect(refreshToken).toBe(null);
   });
+
+  // Mutations:
+  test('Mutation: loginUser', () => {
+    const store = createVuexStore({
+      status: 'authenticating', // 'authenticated', 'not-authenticated', 'authenticating'
+      user: null,
+      idToken: null,
+      refreshToken: null,
+    });
+
+    const payload = {
+      user: { name: 'anbreaker', email: 'test@test.com' },
+      idToken: 'ABC-123',
+      refreshToken: 'XYZ-123',
+    };
+
+    store.commit('auth/loginUser', payload);
+
+    const { status, user, idToken, refreshToken } = store.state.auth;
+
+    expect(status).toBe('authenticated');
+    expect(user).toEqual({ name: 'anbreaker', email: 'test@test.com' });
+    expect(idToken).toBe('ABC-123');
+    expect(refreshToken).toBe('XYZ-123');
+  });
+
+  test('Mutation: logout', () => {
+    const store = createVuexStore({
+      status: 'authenticated', // 'authenticated', 'not-authenticated', 'authenticating'
+      user: { name: 'anbreaker', email: 'test@test.com' },
+      idToken: 'ABC-123',
+      refreshToken: 'XYZ-123',
+    });
+
+    store.commit('auth/logout');
+
+    const { status, user, idToken, refreshToken } = store.state.auth;
+
+    expect(status).toBe('not-authenticated');
+    expect(user).toBe(null);
+    expect(idToken).toBeFalsy();
+    expect(refreshToken).toBeFalsy();
+
+    expect(localStorage.getItem('idToken')).toBeFalsy();
+    expect(localStorage.getItem('refreshToken')).toBeFalsy();
+  });
 });
